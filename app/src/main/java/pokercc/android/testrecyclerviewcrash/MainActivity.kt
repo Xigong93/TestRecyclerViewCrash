67dae4f2852a768b17bbc4594c47dd69d869d8d1
pokercc <pokercc@sina.com>
2020-10-10 17:17:36 +0800

    Fixed notifyItemChange(0) crash.

--- app/src/main/java/pokercc/android/testrecyclerviewcrash/MainActivity.kt
+++ app/src/main/java/pokercc/android/testrecyclerviewcrash/MainActivity.kt
@@ -1,16 +1,16 @@
 package pokercc.android.testrecyclerviewcrash
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import pokercc.android.testrecyclerviewcrash.databinding.ActivityMainBinding
@@ -29,9 +29,12 @@ class MainActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = testAdapter
             testAdapter.setNewData(listOf(0, 1))
-            testAdapter.addHeaderView(ImageView(context).apply {
-                setImageResource(R.mipmap.ic_launcher)
-            })
+            val header = TextView(this@MainActivity).apply {
+                textSize = 28f
+                gravity = Gravity.CENTER
+                text = "Header"
+            }
+            testAdapter.addHeaderView(header)
         }
     }
 }
@@ -41,7 +44,19 @@ private class TestVH(val binding: TestItemBinding) : BaseViewHolder(binding.root
 
 private const val LOG_TAG = "TestAdapter"
 
-private class TestAdapter : BaseQuickAdapter<Int, TestVH>(View.NO_ID) {
+abstract class FixBaseAdapter<T, K : BaseViewHolder>(layoutResId: Int) :
+    BaseQuickAdapter<T, K>(layoutResId) {
+    override fun createBaseViewHolder(view: View): K {
+        (view.parent as? ViewGroup)?.removeView(view)
+        val item = FrameLayout(view.context)
+        item.addView(view)
+        item.layoutParams = recyclerView.generateLayoutParams(null)
+        return super.createBaseViewHolder(item)
+    }
+}
+
+private class TestAdapter : FixBaseAdapter<Int, TestVH>(View.NO_ID) {
+
 
     override fun onCreateDefViewHolder(parent: ViewGroup, viewType: Int): TestVH {
         return LayoutInflater.from(parent.context)
@@ -60,8 +75,11 @@ private class TestAdapter : BaseQuickAdapter<Int, TestVH>(View.NO_ID) {
         holder.binding.text.text = "$position,${System.currentTimeMillis()}"
         holder.itemView.setOnClickListener {
             notifyItemChanged(0)
-            notifyItemChanged(1)
+//            notifyItemChanged(1)
         }
+        // when notify item change without payload. There effect viewHolder will destroy and recreate.
+        // But baseQuickAdapter header and footer use same LinearLayout had old layoutParams will occur crash.
+
     }
 
 }
